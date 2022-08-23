65614ed3f73b9da3562245a399718ca2ba8cbf4e
ImomushiRan <george.villarroel@outlook.es>
2022-08-23 16:29:33 -0300

    Jav:EjerciciosTema1

---
+++ src/com/java/ejercicios/EjerciciosTema1.java
@@ -0,0 +1,36 @@
+package com.java.ejercicios;
+
+public class EjerciciosTema1 {
+
+    public static void main(String[] args) {
+
+        byte num1 = 1;
+        short num2 = 2;
+        int num3 = 4;
+        long num4 = 8;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+
+        float num5 = 2.2F;
+        double num6 = 4.44D;
+
+        System.out.println(num5);
+        System.out.println(num6);
+
+        boolean bool = true;
+        boolean boolf = false;
+
+        System.out.println(bool);
+        System.out.println(boolf);
+
+        char caracter = 'a';
+        String cadena = "Hello World";
+
+        System.out.println(caracter);
+        System.out.println(cadena);
+
+    }
+}
